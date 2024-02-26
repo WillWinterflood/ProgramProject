4bc341b17cb5c694854b436e26b01d156f979a38
WillWinterflood <146846548+WillWinterflood@users.noreply.github.com>
2024-02-26 12:29:22 +0000

    5

---
+++ TestCode.c/Skeleton.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct Maze {
+    int Length;
+    int Line;
+}
+
+struct Controls {
+
+}
+struct StartingPosition {
+
+}
+
+int main {
+
+}
--- TestCode.c/error.sh
+++ TestCode.c/error.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FileName = "Maze1"
+FileName = "ErrorTooShort"
 #This first if statement checks whether the FileName exists 
 if [-f "$FileName"]; then 
     echo "The file '$FileName' exists" 
@@ -13,7 +13,7 @@ fi
 
 #These error messages are checking whether the length of the maze is between 5 and 100 characters
 
-CharCount = $((wc -c < "$FileName"))
+CharCount = $(wc -c < "$FileName")
 
 if (CharCount < 5); then 
     echo "Error: The maze is shorter than 5 characters."
@@ -25,9 +25,43 @@ if (CharCount > 100); then
     exit 1
 fi
 
+#These error codes check the height of the maze is between 5 and 100 characters
 LineCount = $(wc -1 < "$FileName")
 
-if (LineCount)
+if (LineCount < 5); then
+    echo "Error: The maze is less than 5 characters in height."
+    exit 1
+fi
+
+if (LineCount > 100); then
+    echo "Error: The maze is greater than 100 characters in height."
+    exit 1
+fi
+
+#This error checker is cheking if the file has only hashtags,S,E and spaces 
+
+if grep -qE -v '^[# SE]*$' "$FileName"; then
+    echo "Error: The file doesnt contain the right characters"
+    exit 1
+else
+    echo "File has valid characters"
+    exit 0
+fi
+
+#Checking whether they input the correct move
+read -p "AWSD" move 
+
+if grep -q "^$move$" "FileName"; then
+    echo "Valid Move"
+else
+    echo "Invalid Move"
+
+
+
+
+
+
+
 
 
 
